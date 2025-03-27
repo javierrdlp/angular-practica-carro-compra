cad155176681b97d3b471b4235dc75c58a579b2f
Javier <javier_force@hotmail.com>
2025-03-27 12:22:59 +0100

    Desarrollo del carrito, funciones de agregar, eliminar articulos y total

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -16,13 +16,40 @@ export class CartAppComponent implements OnInit {
 
   items: CartItem[] = [];
 
+  total: number = 0;
+
   constructor(private service: ProductService) { }
-  
+
   ngOnInit(): void {
     this.products = this.service.findAll();
   }
 
-  onAddCart(product: Product){
-    this.items = [... this.items, { product: {...product}, quantity: 1 }];
+  onAddCart(product: Product): void {
+    const hasItem = this.items.find(item => item.product.id == product.id);
+
+    if (hasItem) {
+      this.items = this.items.map(item => {
+        if (item.product.id == product.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1
+          }
+        }
+        return item;
+      });
+    } else {
+      this.items = [... this.items, { product: { ...product }, quantity: 1 }];
+    }
+
+    this.calculateTotal();
+  }
+
+  onDeleteCart(id: number): void{
+    this.items = this.items.filter(item => item.product.id != id);
+    this.calculateTotal();
+  }
+
+  calculateTotal(): void{
+    this.total = this.items.reduce((total, item) => total + (item.quantity * item.product.price), 0);
   }
 }
