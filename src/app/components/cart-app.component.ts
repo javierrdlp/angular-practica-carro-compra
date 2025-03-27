f9c06dd069b330a921e6c4b0d9ecab6ff5fef8ff
Javier <javier_force@hotmail.com>
2025-03-27 08:59:46 +0100

    Catálogo terminado

---
+++ src/app/components/cart-app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItem';
+
+@Component({
+  selector: 'cart-app',
+  imports: [CatalogComponent, CartComponent],
+  templateUrl: './cart-app.component.html'
+})
+export class CartAppComponent implements OnInit {
+
+  products: Product[] = [];
+
+  items: CartItem[] = [];
+
+  constructor(private service: ProductService) { }
+  
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
+
+  onAddCart(product: Product){
+    this.items = [... this.items, { product: {...product}, quantity: 1 }];
+  }
+}
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Ratón Razer Naga',
+        description: 'Ratón negro con iliminación led verde',
+        price: 120
+    },
+    {
+        id: 2,
+        name: 'Teclado Logitech Membrana',
+        description: 'Teclado 60% inalámbrico',
+        price: 50
+    },
+    {
+        id: 3,
+        name: 'Alfombrilla Steelseries',
+        description: 'Alfombrilla negra mate',
+        price: 22
+    },
+    {
+        id: 4,
+        name: 'Teclado Razer Mecánico',
+        description: 'Teclado mecanico Racer Switch-Brown',
+        price: 180
+    },
+    {
+        id: 5,
+        name: 'Monito Benq HDR',
+        description: 'Monitor 27" Full HD',
+        price: 350
+    },
+    {
+        id: 6,
+        name: 'Silla Gamer',
+        description: 'Silla reclinable, 4 puntos de ajuste',
+        price: 250
+    }
+]
