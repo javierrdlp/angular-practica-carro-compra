9bd956c9285ade242ee790d3d4ae92117bb42715
Javier <javier_force@hotmail.com>
2025-04-01 12:10:51 +0200

    Modificación para usar rutas en diferentes páginas y uso de biblioteca SweetAlert2

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,74 +1,105 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/product.service';
-import { Product } from '../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartItem } from '../models/cartItem';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ModalCartComponent } from './modal-cart/modal-cart.component';
+import { Router, RouterOutlet } from '@angular/router';
+import { SharingDataService } from '../services/sharing-data.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'cart-app',
-  imports: [CatalogComponent, NavbarComponent, ModalCartComponent],
+  imports: [CatalogComponent, NavbarComponent, RouterOutlet],
   templateUrl: './cart-app.component.html'
 })
-export class CartAppComponent implements OnInit {
-
-  products: Product[] = [];
+export class CartAppComponent implements OnInit {  
 
   items: CartItem[] = [];
 
- // total: number = 0;
+  total: number = 0;
 
-  showCart: boolean = false;
+  constructor(
+    private router: Router,
+    private sharingDataService: SharingDataService) { }
 
-  constructor(private service: ProductService) { }
-
-  ngOnInit(): void {
-    this.products = this.service.findAll();    
+  ngOnInit(): void {   
     this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
-    // this.calculateTotal();
+    this.calculateTotal();
+    this.onDeleteCart(); // ponemos esto aquí para suscribirse al idProductEventEmitter 
+    this.onAddCart(); // ponemos esto aquí para suscribirse al productEventEmitter
   }
 
-  onAddCart(product: Product): void {
-    const hasItem = this.items.find(item => item.product.id === product.id);
-    
-    if (hasItem) {
-      this.items = this.items.map(item => {
-        if (item.product.id === product.id) {
-          return {
-            ...item,
-            quantity: item.quantity + 1
+  onAddCart(): void {
+    this.sharingDataService.productEventEmitter.subscribe(product => {
+
+      const hasItem = this.items.find(item => item.product.id === product.id);
+      if (hasItem) {
+        this.items = this.items.map(item => {
+          if (item.product.id === product.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1
+            }
           }
+         
+          return item;          
+        })       
+      } else {
+        this.items = [... this.items, { product: { ...product }, quantity: 1 }];
+      }
+      Swal.fire({
+        position: "top",
+        icon: "success",
+        title: product.name + " añadido al carro",
+        showConfirmButton: false,
+        timer: 1500,
+        width: "20%"
+      });
+      sessionStorage.setItem('cart', JSON.stringify(this.items));
+      this.saveSession();
+      this.calculateTotal();      
+    });
+  }
+
+  onDeleteCart(): void {    
+    this.sharingDataService.idProductEventEmitter.subscribe(id => {
+      this.items = this.items.filter(item => item.product.id !== id);
+      Swal.fire({
+        title: "¿Estás seguro?",
+        text: "No hay vuelta atrás",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Eliminar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          if (this.items.length === 0) {
+            sessionStorage.removeItem('cart');
+          }
+          this.saveSession();
+          this.calculateTotal();
+    
+          this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+            this.router.navigate(['/cart'], {
+              state: {items: this.items, total: this.total}
+            })
+          })           
+          Swal.fire({
+            title: "¡Eliminado!",
+            text: "Tu producto se ha eliminaod del carro.",
+            icon: "success"
+          });
         }
-        return item;
-      })
-    } else {
-      this.items = [... this.items, { product: { ...product }, quantity: 1 }];
-    }
-   
+      });
+        
+    })
+  }
+
+  calculateTotal(): void {
+    this.total = this.items.reduce((total, item) => total + (item.quantity * item.product.price), 0);
+  }
+
+  saveSession(): void {
     sessionStorage.setItem('cart', JSON.stringify(this.items));
-    // this.saveSession();
-    // this.calculateTotal();
-  }
-
-  onDeleteCart(id: number): void{
-    this.items = this.items.filter(item => item.product.id !== id);
-    if(this.items.length === 0){
-      sessionStorage.removeItem('cart');
-    }
-    // this.saveSession();
-    // this.calculateTotal();
-  }
-
-  // calculateTotal(): void{
-  //   this.total = this.items.reduce((total, item) => total + (item.quantity * item.product.price), 0);
-  // }
-
-  // saveSession(): void {
-  //   sessionStorage.setItem('cart', JSON.stringify(this.items));
-  // }
-
-  openCloseCart(): void{
-    this.showCart = !this.showCart;
   }
 }
--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { SharingDataService } from '../../services/sharing-data.service';
+import { Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'catalog',
@@ -8,13 +11,22 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   templateUrl: './catalog.component.html'
  
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
 
-  @Input() products: Product[] = [];
+  products: Product[] = [];
 
-  @Output() productEventEmitter: EventEmitter<Product> = new EventEmitter();
+
+  constructor(
+    private productService: ProductService,
+    private sharingDataService: SharingDataService){
+   
+
+  }
+  ngOnInit(): void {
+    this.products = this.productService.findAll();
+  }
 
   onAddCart(product: Product){
-    this.productEventEmitter.emit(product);    
+    this.sharingDataService.productEventEmitter.emit(product);    
   }
 }
